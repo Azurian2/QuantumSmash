398c211b06a91e2a0724d3270b88bc8421ef1dce
Icy-Dragon <ethanandrewcoleman@gmail.com>
2016-07-31 20:35:05 -0500

    Item Update - Pre3 unstable version
    Added a background, broke the popup damage text in the process though,
    will be fixed.

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    public float cameraDistOffset = 10;
+    private GameObject background;
+    private GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        background = GameObject.Find("background");
+        player = GameObject.Find("Ship");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerInfo = player.transform.transform.position;
+        background.transform.position = new Vector3(playerInfo.x, playerInfo.y, playerInfo.z - cameraDistOffset);
+    }
+}
